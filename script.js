6c4f0c461727aa0225bc5e5e97a952afe041df66
joshuadorley <joshuaboateng33@gmail.com>
2025-02-16 20:50:32 -0500

    update

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== Typed JS ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Full-Stack Developer'],
+    strings: ['FullStack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
